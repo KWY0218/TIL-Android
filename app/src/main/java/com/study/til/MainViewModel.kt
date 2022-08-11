a0f5f4d50a82193cc7d918dbdceec33570dedad3
KWY0218 <wodrk1001@gmail.com>
2022-08-11 22:13:56 +0900

    [mod] viewmodel flow 수집 방법 수정

--- app/src/main/java/com/study/til/MainViewModel.kt
+++ app/src/main/java/com/study/til/MainViewModel.kt
@@ -4,14 +4,10 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.study.til.usecase.CalculateDateUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
-import kotlinx.coroutines.delay
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
@@ -22,47 +18,45 @@ class MainViewModel @Inject constructor(
 
     init {
         viewModelScope.launch {
-            fetchBoardList()
-                .collect { result ->
-                    val tempResult = mutableListOf<Board>()
-                    result.forEach { board ->
-                        var tempBoard = Board()
-                        calculateDateUseCase(board.writeDate).collect {
-                            tempBoard = board.copy(time = it)
-                        }
-                        tempResult.add(tempBoard)
-                    }
-                    _boardList.value = tempResult
-                }
+            _boardList.value = fetchBoardList()
+            refreshTime()
         }
     }
 
-    private fun fetchBoardList(): Flow<List<Board>> = flow {
-        delay(300)
-        emit(
-            listOf(
-                Board(
-                    id = 1,
-                    writer = "맑은비",
-                    title = "몽실 베이커리",
-                    writeDate = "2022-08-11 20-55-22",
-                    content = "첫번째 케이스"
-                ),
-                Board(
-                    id = 2,
-                    writer = "맑은비",
-                    title = "몽실 베이커리",
-                    writeDate = "2022-08-11 15-20-22",
-                    content = "두번째 케이스"
-                ),
-                Board(
-                    id = 3,
-                    writer = "맑은비",
-                    title = "몽실 베이커리",
-                    writeDate = "2022-08-10 19-20-22",
-                    content = "세번째 케이스"
-                )
+    private suspend fun refreshTime() {
+        val tempBoardList = mutableListOf<Board>()
+        boardList.value.forEach { board ->
+            var tempBoard = Board()
+            calculateDateUseCase(board.writeDate).collect { timeText ->
+                tempBoard = board.copy(time = timeText)
+            }
+            tempBoardList.add(tempBoard)
+        }
+        _boardList.value = tempBoardList
+    }
+
+    private fun fetchBoardList(): List<Board> =
+        listOf(
+            Board(
+                id = 1,
+                writer = "맑은비",
+                title = "몽실 베이커리",
+                writeDate = "2022-08-11 21-55-22",
+                content = "첫번째 케이스"
+            ),
+            Board(
+                id = 2,
+                writer = "맑은비",
+                title = "몽실 베이커리",
+                writeDate = "2022-08-11 15-20-22",
+                content = "두번째 케이스"
+            ),
+            Board(
+                id = 3,
+                writer = "맑은비",
+                title = "몽실 베이커리",
+                writeDate = "2022-08-10 19-20-22",
+                content = "세번째 케이스"
             )
         )
-    }
 }
