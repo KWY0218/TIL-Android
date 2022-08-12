dfda551823ff89a9653d8c2ac75a15ba6530595f
KWY0218 <wodrk1001@gmail.com>
2022-08-13 00:58:40 +0900

    combine jetpack navigation with bottom navigation

--- app/src/main/java/com/study/til/MainActivity.kt
+++ app/src/main/java/com/study/til/MainActivity.kt
@@ -2,10 +2,20 @@ package com.study.til
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.study.til.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.main_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.mainBotNav.setupWithNavController(navController)
     }
 }
