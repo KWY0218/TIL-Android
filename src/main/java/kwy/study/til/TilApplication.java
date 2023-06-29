4350f2a2dc901aa6566e563f4be7222cbe24b00f
KWY <wodrk1001@gmail.com>
2023-06-30 01:42:29 +0900

    cors error

--- src/main/java/kwy/study/til/TilApplication.java
+++ src/main/java/kwy/study/til/TilApplication.java
@@ -4,16 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(
-		exclude = {
-				org.springframework.cloud.aws.autoconfigure.context.ContextInstanceDataAutoConfiguration.class,
-				org.springframework.cloud.aws.autoconfigure.context.ContextStackAutoConfiguration.class,
-				org.springframework.cloud.aws.autoconfigure.context.ContextRegionProviderAutoConfiguration.class
-		}
+        exclude = {
+                org.springframework.cloud.aws.autoconfigure.context.ContextInstanceDataAutoConfiguration.class,
+                org.springframework.cloud.aws.autoconfigure.context.ContextStackAutoConfiguration.class,
+                org.springframework.cloud.aws.autoconfigure.context.ContextRegionProviderAutoConfiguration.class
+        }
 )
 public class TilApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TilApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TilApplication.class, args);
+    }
 
 }
