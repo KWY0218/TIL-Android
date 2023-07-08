d8dd16fab6445b855de722ce0609fec72b0ed0d5
KWY <wodrk1001@gmail.com>
2023-07-09 03:30:45 +0900

    add

--- src/main/java/kwy/study/til/domain/User.java
+++ src/main/java/kwy/study/til/domain/User.java
@@ -1,11 +1,13 @@
 package kwy.study.til.domain;
 
 
+import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -20,6 +22,10 @@ public class User {
 
     @Column(nullable = false)
     private String name;
+
+    @Column
+    @OneToMany
+    private List<Product> products;
 
     public User(String name) {
         this.name = name;
